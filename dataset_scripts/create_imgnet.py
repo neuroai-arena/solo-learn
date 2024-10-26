e05b586274c29c239a24ff2f4e5f2b892d312a93
ts <schaumloeffel.tim@web.de>
2024-10-26 09:32:00 +0200

    Add file to create clean ImageNet h5

---
+++ dataset_scripts/create_imgnet.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+import h5py
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+dataset_root = Path("/pfss/mlde/workspaces/mlde_wsp_PI_Roig/shared/datasets/")
+output_dir = dataset_root / "ImageNet" / "h5"
+output_dir.mkdir(exist_ok=True, parents=True)
+
+
+# get the official ImageNet class map and class names
+class_map_url = "https://raw.githubusercontent.com/raghakot/keras-vis/refs/heads/master/resources/imagenet_class_index.json"
+class_map = pd.read_json(class_map_url, orient="index").reset_index().rename(
+    columns={"index": "class_index", 0: "class_wn", 1: "class_name"})
+class_map["class_index"] = class_map["class_index"].astype(int)
+assert len(class_map) == 1000
+assert set(class_map["class_index"]) == set(range(1000))
+
+class_wn_2_class_index = class_map.set_index("class_wn")["class_index"].to_dict()
+class_wn_2_class_name = class_map.set_index("class_wn")["class_name"].to_dict()
+
+for split in ["train", "val"]:
+    with h5py.File(output_dir / f"ImageNet_{split}.h5", "w") as h5_file:
+        mapper = []
+
+        image_filenames = list((dataset_root / "ImageNet" / split).rglob("*.JPEG"))
+        cat_bar = tqdm(image_filenames, desc=f"{split} split")
+
+        dtype = h5py.vlen_dtype(np.dtype('uint8'))
+        image_dataset = h5_file.create_dataset(f"images", shape=(len(image_filenames),), dtype=dtype)
+
+        for i, image_filename in enumerate(cat_bar):
+            image_dataset[i] = np.fromfile(image_filename, dtype=np.uint8)
+            mapper.append(
+                {
+                    'h5_index': i,
+                    "filename": image_filename.name,
+                    "wn_name": image_filename.parent.stem
+                }
+            )
+    mapper = pd.DataFrame(mapper)
+    mapper['target'] = mapper['wn_name'].apply(lambda x: class_wn_2_class_index[x])
+    mapper['class_name'] = mapper['wn_name'].apply(lambda x: class_wn_2_class_name[x])
+    mapper.to_parquet(output_dir / f"ImageNet_{split}_mapper.parquet", index=False)
--- solo/data/custom/imagenet.py
+++ solo/data/custom/imagenet.py
@@ -1,6 +1,8 @@
 import io
 import json
 import os
+from pathlib import Path
+from typing import Union, Callable, Optional, Tuple
 
 import h5py
 import pandas as pd
@@ -14,13 +16,13 @@ class ImgnetDataset(Dataset):
         # super().__init__()
         self.mode = "train" if split == "train" else "val"
         driver = "core" if self.mode == "train" and imgnet100 else None
-        self.hdf5_file = h5py.File(os.path.join(data_root, f'data2_{self.mode}.h5'),"r", driver=driver)
+        self.hdf5_file = h5py.File(os.path.join(data_root, f'data2_{self.mode}.h5'), "r", driver=driver)
         self.dataset = pd.read_csv(os.path.join(data_root, f'dataset2_{self.mode}.csv'), header=None)
         self.dataset.columns.astype(str)
-        self.dataset.columns = ["index","0", "1", "2", "label", "4"] if self.mode == "train" else ["0", "1", "2", "label", "4"]
+        self.dataset.columns = ["index", "0", "1", "2", "label", "4"] if self.mode == "train" else ["0", "1", "2",
+                                                                                                    "label", "4"]
         self.transform = transform
         self.n_classes = 1000
-
 
         self.imgnet100 = imgnet100
         if imgnet100:
@@ -39,12 +41,11 @@ class ImgnetDataset(Dataset):
             self.dataset = self.dataset.query("label in @catfilter")
             self.dataset = self.dataset.reset_index(drop=True)
 
-
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, idx):
-        h5_index, label, begin  = self.dataset.loc[idx,"0"], self.dataset.loc[idx,"label"], self.dataset.loc[idx,'4']
+        h5_index, label, begin = self.dataset.loc[idx, "0"], self.dataset.loc[idx, "label"], self.dataset.loc[idx, '4']
 
         if self.imgnet100:
             label = self.category_mapping[label]
@@ -52,7 +53,49 @@ class ImgnetDataset(Dataset):
         if self.mode == "val":
             img = Image.open(io.BytesIO(self.hdf5_file.get(f"data2_{begin}")[h5_index]))
         else:
-            h5_index = h5_index%50000
+            h5_index = h5_index % 50000
             img = Image.open(io.BytesIO(self.hdf5_file.get(str(begin)).get("data")[h5_index]))
         x = self.transform(img)
         return x, label
+
+
+class ImgNetDataset_42(Dataset):
+    def __init__(
+            self,
+            root: Union[str, Path],
+            transform: Optional[Callable] = None,
+            split: str = "train",
+            imgnet100: bool = False
+    ):
+        self.root = Path(root)
+        self.split = split
+        self.transform = transform
+        assert self.split in ["train", "val"]
+
+        self.h5_file = h5py.File(root / f"ImageNet_{split}.h5", "r")
+        self.mapper = pd.read_parquet(root / f"ImageNet_{split}_mapper.parquet")
+
+        if imgnet100:
+            with open(root / 'imagenet100_classes.txt') as f:
+                imgnet100_classes = sorted(f.readline().strip().split())
+            imgnet100_class_wn_2_class_index = {class_wn: class_index for class_index, class_wn in
+                                                enumerate(imgnet100_classes)}
+
+            self.mapper = self.mapper.query('wn_name in @imgnet100_classes').reset_index(drop=True)
+            self.mapper['target'] = self.mapper['wn_name'].apply(lambda x: imgnet100_class_wn_2_class_index[x])
+
+        self.n_classes = self.mapper['target'].nunique()
+        self.target_2_class_name = self.mapper[['target', 'class_name']].drop_duplicates().set_index('target')[
+            'class_name'].to_dict()
+
+    def __len__(self) -> int:
+        return len(self.mapper)
+
+    def __getitem__(self, idx: int) -> Tuple[Image.Image, int]:
+        dp = self.mapper.loc[idx]
+        image = Image.open(io.BytesIO(self.h5_file.get("images")[dp["h5_index"]]))
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, dp["target"]
